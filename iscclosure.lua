d6c669e1c5bb92ba94eecaa7084439730c4a315b
MrY7zz <99chinesesteve@gmail.com>
2025-06-27 00:50:50 -0300

    Create iscclosure.lua

---
+++ iscclosure.lua
@@ -0,0 +1,21 @@
+-- iscclosure.lua
+-- © 2025 MrY7zz (MIT License)
+
+local debug_info = debug.info
+
+function iscclosure(func)
+	if typeof(func) ~= "function" then
+		error("invalid argument #1 to 'iscclosure' (function expected, got " .. typeof(func) .. ")")
+	end
+	return debug_info(func, "s") == "[C]"
+end
+
+--[=[
+Usage:
+
+local emptyfunction = function() end
+local twait = task.wait
+
+print(iscclosure(twait)) --> Outputs "true"
+print(iscclosure(emptyfunction)) --> Outputs "false"
+]=]
