4461f09d9f275bce71ffdf27b42a18012c5eb9ce
MrY7zz <99chinesesteve@gmail.com>
2026-02-28 16:59:59 -0300

    Update gethiddenproperty.lua

--- gethiddenproperty.lua
+++ gethiddenproperty.lua
@@ -8,31 +8,30 @@ local GetPropertyValue = UGCValidationService.GetPropertyValue
 local FindFirstChild = game.FindFirstChild
 local __Index
 xpcall(function()
-    return game[{}]
+	return game[{}]
 end, function()
-    __Index = debug.info(2, 'f')
+	__Index = debug.info(2, 'f')
 end)
 
 function gethiddenproperty(instance, property)
-    --// First we try to get the property with UGCValidationService
-    --// This won't work 100% of the times
-    local success, result = pcall(function()
-        return GetPropertyValue(UGCValidationService, instance, property)
-    end)
-    if success and result ~= nil then
-        return result, true
-    else
-        if FindFirstChild(instance, property) ~= nil then return nil end --// Prevent indexing an instance instead of a property
-        --// If it didn't return anything, or it didn't succeed,
-        --// We try accessing it directly
-         local success2, result2 = pcall(__Index, instance, property)
-        if success2 then
-            return result2, false
-        else
-            return __Index(instance, property)
-        end
-    end
-    return nil
+	--// First we try to get the property with UGCValidationService
+	--// This won't work 100% of the times
+	local success, result = pcall(function()
+		return GetPropertyValue(UGCValidationService, instance, property)
+	end)
+	if success and result ~= nil then
+		return result, true
+	else
+		if FindFirstChild(instance, property) ~= nil then return nil end --// Prevent indexing an instance instead of a property
+		--// If it didn't return anything, or it didn't succeed,
+		--// We try accessing it directly
+		local success2, result2 = pcall(__Index, instance, property)
+		if success2 then
+			return result2, false
+		else
+			return __Index(instance, property)
+		end
+	end
 end
 
 --[[
